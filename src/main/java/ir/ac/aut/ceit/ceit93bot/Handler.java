7c152ba818efc0879dcbe0d0061f604bb32f9ca7
Iman Tabrizian <tabrizian@outlook.com>
2016-04-15 17:04:03 +0430

    Listener loop complete.

--- src/main/java/ir/ac/aut/ceit/ceit93bot/Handler.java
+++ src/main/java/ir/ac/aut/ceit/ceit93bot/Handler.java
@@ -1,17 +1,22 @@
 package ir.ac.aut.ceit.ceit93bot;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class Handler implements Runnable {
 
 	private HttpClient httpClient;
 	private JSONArray updates;
+	private JSONParser parser = new JSONParser();
 
 	public Handler(HttpClient httpClient) {
 		this.httpClient = httpClient;
@@ -31,7 +36,16 @@ public class Handler implements Runnable {
 							"Failed : HTTP error code : " + response.getStatusLine().getStatusCode());
 				}
 
-				updates = (JSONArray) response.getEntity();
+				try {
+					if (response.getEntity().getContent() != null)
+						setUpdates((JSONArray) parser.parse(new InputStreamReader(response.getEntity().getContent())));
+				} catch (IllegalStateException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (ParseException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 
 			} catch (ClientProtocolException e) {
 
@@ -42,6 +56,14 @@ public class Handler implements Runnable {
 				e.printStackTrace();
 			}
 		}
+	}
+
+	public JSONArray getUpdates() {
+		return updates;
+	}
+
+	public void setUpdates(JSONArray updates) {
+		this.updates = updates;
 	}
 
 }
