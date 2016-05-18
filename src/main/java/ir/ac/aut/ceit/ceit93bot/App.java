028726fba21b062db531f58321cc02f3c98fa9c0
Iman Tabrizian <tabrizian@outlook.com>
2016-05-19 01:01:24 +0430

    Adds subject listening

--- src/main/java/ir/ac/aut/ceit/ceit93bot/App.java
+++ src/main/java/ir/ac/aut/ceit/ceit93bot/App.java
@@ -3,6 +3,9 @@ package ir.ac.aut.ceit.ceit93bot;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * Hello world!
  */
@@ -10,36 +13,67 @@ public class App {
     public static void main(String[] args) {
         Server server = new Server();
         CeitBot ceitBot = new CeitBot();
+        ArrayList<String> subjects = new ArrayList<>();
+        ArrayList<ArrayList<Long>> associatedUserIds = new ArrayList<>();
         while (true) {
             try {
-
                 while (server.getUpdates().size() > 0) {
                     boolean send = true;
 
                     JSONObject update = (JSONObject) ((JSONArray) server.getUpdates().get(0)).get(0);
                     JSONObject message = (JSONObject) update.get("message");
                     JSONArray entities = (JSONArray) message.get("entities");
-                    if (entities != null)
-                        for (Object entity :
-                                entities) {
-                            if (((JSONObject) entity).get("type").equals("bot_command")) {
-                                send = false;
-                                if (((String) message.get("text")).equals("/sendmessage")) {
-                                    JSONObject chat = (JSONObject) message.get("chat");
-                                    Long id = (Long) chat.get("id");
-                                    ceitBot.sendMessage(id, "باتشکر از پیام شما، پیام شما برای ما ارسال شد.");
-                                }
+                    String messageText = (String) message.get("text");
+                    Long chatId = (Long) ((JSONObject) message.get("chat")).get("id");
+                    if (messageText.startsWith("--")) {
+                        int requestSubject = -1;
+                        for (int i = 0; i < subjects.size(); i++) {
+                            if (messageText.contains("--" + subjects.get(i))) {
+                                requestSubject = i;
+                                break;
                             }
                         }
-                    if (send) {
-                        long fromChatId = (long) ((JSONObject) message.get("chat")).get("id");
-                        long messageId = (long) (message.get("message_id"));
-                        ceitBot.forwardMessageToAdmins(fromChatId, messageId);
 
+                        if (requestSubject == -1) {
+                            ArrayList<Long> userIds = new ArrayList<>();
+                            associatedUserIds.add(userIds);
+                            String firstLine = messageText.split("\n")[0];
+                            subjects.add(firstLine.substring(2, firstLine.length()));
+                            userIds.add(chatId);
+                            requestSubject = subjects.size() - 1;
+                        } else {
+                            associatedUserIds.get(requestSubject).add(chatId);
+                        }
+
+                        for (Long userId:
+                             associatedUserIds.get(requestSubject)) {
+                            ceitBot.forwrardMessage(userId, chatId, (Long) message.get("message_id"));
+                        }
+                    } else {
+
+
+                        if (entities != null)
+                            for (Object entity :
+                                    entities) {
+                                if (((JSONObject) entity).get("type").equals("bot_command")) {
+                                    send = false;
+                                    if (((String) message.get("text")).equals("/sendmessage")) {
+                                        JSONObject chat = (JSONObject) message.get("chat");
+                                        Long id = (Long) chat.get("id");
+                                        ceitBot.sendMessage(id, "باتشکر از پیام شما، پیام شما برای ما ارسال شد.");
+                                    }
+                                }
+                            }
+                        if (send) {
+                            long fromChatId = (long) ((JSONObject) message.get("chat")).get("id");
+                            long messageId = (long) (message.get("message_id"));
+                            ceitBot.forwardMessageToAdmins(fromChatId, messageId);
+
+                        }
+                        server.getUpdates().remove(0);
                     }
-                    server.getUpdates().remove(0);
-                }
 
+                }
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
                 // TODO Auto-generated catch block
@@ -47,4 +81,6 @@ public class App {
             }
         }
     }
+
+
 }
